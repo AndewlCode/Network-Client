f38496f7c69e4b7b516e8b5d8ed75ab0707b77e5
Andrey Lobachev <andrey_lobachev@internet.ru>
2023-04-29 18:15:22 +0300

    Client logic changed

--- src/main/java/NetworkClient.java
+++ src/main/java/NetworkClient.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 
 public class NetworkClient {
     private final String host;
@@ -14,12 +15,32 @@ public class NetworkClient {
     }
 
     public void start() throws Exception {
-        try (Socket clienSocket = new Socket(this.host, this.port);
-             PrintWriter out = new PrintWriter(clienSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clienSocket.getInputStream()))) {
-            out.println("George");
-            String response = in.readLine();
-            System.out.println(response);
+        for (int i = 0; i < 10; i++) {
+            try (Socket clienSocket = new Socket(this.host, this.port);
+                 PrintWriter out = new PrintWriter(clienSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clienSocket.getInputStream()))) {
+
+                String command = getRandomCommand();
+                out.println(command);
+                String response = in.readLine();
+                System.out.println(response);
+            }
         }
+    }
+
+    // Генерация случайных команд для сервера
+    public String getRandomCommand() {
+        String[] commands = new String[]{
+                "Hi",
+                "Say Your name",
+                "What date is it today",
+                "What is yours favourite movie",
+                "How old are you?",
+                "Bye",
+                "Where are you?",
+                "UNKNOWN QUESTION"
+        };
+        Random random = new Random();
+        return commands[random.nextInt(commands.length)];
     }
 }
